dba6556a946d3be9d7cc9c827fb52bfa24f5c1e6
Kalle Møller <github.com@k-moeller.dk>
2015-08-27 14:57:56 +0200

    Update InstallPlugins.sh

--- InstallPlugins.sh
+++ InstallPlugins.sh
@@ -14,6 +14,9 @@ java -jar jenkins-cli.jar -s http://127.0.0.1:8080 install-plugin git
 echo install msbuild plugin
 java -jar jenkins-cli.jar -s http://127.0.0.1:8080 install-plugin msbuild
 
+echo install github plugin
+java -jar jenkins-cli.jar -s http://127.0.0.1:8080 install-plugin github
+
 echo restart jenkins
 java -jar jenkins-cli.jar -s http://127.0.0.1:8080 restart
 
